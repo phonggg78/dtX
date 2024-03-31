4ddc70d042da90e88040fe07060365db1ad8fe04
phonggg78 <98084407+phonggg78@users.noreply.github.com>
2024-03-31 09:33:49 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH20022731 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH20022731'),
+  ('Big Data', 'IUH20022731'),
+  ('Cloud Deployement', 'IUH20022731'),
+('Data Analysis', 'IUH20022731'),
+('Block Chain', 'IUH20022731');
